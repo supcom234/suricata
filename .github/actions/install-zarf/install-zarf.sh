951f7741b7d270502e460a836736f7527556d2e2
ablanchard <andrew.blanchard@leapfrog.ai>
2022-12-07 18:27:14 -0500

    Refactoring the pipeline to standardize across repos

--- .github/actions/install-zarf/install-zarf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-set -x
-
-curl -s -O -L https://github.com/defenseunicorns/zarf/releases/download/v0.22.2/zarf_v0.22.2_Linux_amd64
-sudo install -g root -o root -m 0777 zarf_v0.22.2_Linux_amd64 /usr/local/bin/zarf
-zarf version
