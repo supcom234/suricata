bc9c4b1942c20a3ee431704d5d29bba262fe1699
ablanchard <andrew.blanchard@leapfrog.ai>
2022-12-07 15:29:01 -0500

    Refactoring vars/consts

--- .github/actions/package-create/package-create.sh
+++ .github/actions/package-create/package-create.sh
@@ -8,4 +8,4 @@ else
 fi
 
 set -x
-zarf package create --set BRANCH="$BRANCH" --set REPOSITORY=registry.hub.docker.com/jasonish/suricata --set TAG=6.0.2 --set INTERFACE=eth0 --set SURICATA_LOG_PATH=/data/suricata --set NODE_LABEL=kubernetes.io/os --set NODE_LABEL_VALUE=linux --confirm --skip-sbom
+zarf package create --set BRANCH="$BRANCH" --confirm --skip-sbom
